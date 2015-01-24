144b71c2c7cbba44696f871b06c8997af83e6e05
Carlo88 <Carlo88@users.noreply.github.com>
2015-01-24 19:33:44 +0100

    Update Tidy_dataset.R

--- Tidy_dataset.R
+++ Tidy_dataset.R
@@ -6,11 +6,15 @@ Tidy_dataset<-function(){
   data<-rbind(train_set,test_set) #Bind rows
   features<-read.table("./features.txt") #Load Features (column names)
   names(data)<-as.character(features$V2) #Add column names to dataset
+  
+  Data_1<-data #Data 1 contains the complete dataset
   #End task1. Now, I have the complete dataset (test+train sets with column names)
 
   #2.Extracts only the measurements on the mean and standard deviation for each measurement.
   position<-grep("mean|std",names(data)) #Index position of colomn names that contain the words "mean" or "std"
   data2<-data[position] #I extract the column that have names including "mean"or"std"
+  
+  Dataset_only_mean_std<-data2
   
   #3.Uses descriptive activity names to name the activities in the data set
   activity_train<-read.table("./train/y_train.txt") #Extract y_train,a file containing activities names
@@ -20,13 +24,19 @@ Tidy_dataset<-function(){
   activity<-as.character(activity) #cast to character vectors
   activity<-factor(activity,labels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")) #converts number into activity names
   activity<-as.character(activity) #cast to character
-  data3<-cbind(data2,activity)  #column binding
+  data3<-cbind(data2,activity) #column binding
+  DataWithActNames<-data3 #Point 3 completed
   
   #4.Appropriately labels the data set with descriptive variable names. 
-  c<-colnames(data3) #assigns col names to c
-  c<-gsub('-mean','Mean',c) #substitute mean with Mean 
-  c<-gsub('-std','SD',c) #std with SD
-  c<-gsub('-()','',c) #eliminate ()  
+  nam<-colnames(data3) #assigns col names to c 
+  f<-function(x){
+    x<-gsub('-mean',' Mean ',x) #substitute mean with Mean 
+    x<-gsub('-std',' Stand.Dev ',x) #std with SD
+    if (length(grep("^t",x))>0) gsub('t','Time Domain - ',x) #Substitute t with Time Domain
+    if (length(grep("^f",x))>0) gsub('f','Frequency Domain -',x) #Substitute f with Frequency Domain                 
+  }
+  nam<-f(nam)
+  
   
   #5.From the data set in step 4, creates a second, 
   # independent tidy data set with the average of each variable 
